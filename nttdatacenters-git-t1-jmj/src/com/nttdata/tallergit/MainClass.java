185c6a64856d7953d9ae4a56465f880fe2022689
jtellezr <juan.alejandro.tellez.rubio@emeal.nttdata.com>
2023-04-17 14:18:22 +0200

    Corrección - NTT Data - JATR

---
+++ nttdatacenters-git-t1-jmj/src/com/nttdata/tallergit/MainClass.java
@@ -0,0 +1,21 @@
+package com.nttdata.tallergit;
+/**
+ * Clase principal
+ * 
+ * @author NTT Data
+ *
+ */
+public class MainClass {
+
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Impresión
+		System.out.println("Hola Mundo");
+		System.out.println("Hola NTT Data");
+	}
+}
